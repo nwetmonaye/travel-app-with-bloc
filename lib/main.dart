3087bac96ead1f308882534f64352c2ea4f0a9d3
Nwet Mon Aye <nwetmonaye@bimgoc.com>
2024-03-19 22:24:44 +0630

    nma

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: BlocProvider<AppCubit>(
         create: (context) => AppCubit(
           data: DataServices(),
